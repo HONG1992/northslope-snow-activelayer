14eb6271613ba48738a93908768224751e61a57c
Ksenia Lepikhina <kklepi@gmail.com>
2017-03-16 14:59:07 -0600

    Added permafrost data and wrote the code for looking at the snow extent where the active point data is

--- snow_seasonality_proc.R
+++ snow_seasonality_proc.R
@@ -130,8 +130,174 @@ for (i in 1:length(fileNames))
   #check if it did anything
   show(i)
 }
+
+#Write dataframes to csv files
 setwd("~/Desktop/modis_data/MODISSnowSeasonality/New/dataFrames/")
 write.csv(fsSnow, file = "fsSnow.csv")
 write.csv(lsSnow, file = "lsSnow.csv")
 write.csv(fsSnowCont, file = "fsSnowCont.csv")
 write.csv(lsSnowCont, file = "lsSnowCont.csv")
+
+
+
+#-------------------------------------------------------------------------------------------------------------
+#Analysis of Permafrost Data
+#Earth Lab - Project Permafrost
+#By: Ksenia Lepikhina and Jeffery Thompson (mentor)
+#Copy Right
+
+library(sp)
+library(rgdal)
+library(raster)
+library(rgeos)
+
+#open folder
+setwd("~/Desktop/modis_data/calm_sites/CALM_SitesData_Cleaned")
+dataBaseDir <- c("~/Desktop/")
+listDir <- paste(dataBaseDir,c("modis_data/calm_sites/CALM_SitesData_Cleaned/"),sep="")
+boundDir <- paste(dataBaseDir,c("modis_data/calm_sites/CALM_SitesData_Cleaned//LCC_Arctic_Alaska_Yukon/"),sep="")
+
+#Load North Slope shape file
+NS <- readOGR(dsn=path.expand(paste(boundDir, c("LCC_Arctic_Alaska_Yukon.shp"),sep="")),layer ="LCC_Arctic_Alaska_Yukon")
+
+#Load active layer shape file
+activeIn <- shapefile(paste(listDir,c("CALM_SitesData_Cleaned.shp"),sep=""),stringsAsFactors=FALSE)
+
+ntSlopeProj <-projection(NS)
+activeAK <- spTransform(activeIn,ntSlopeProj)
+activeAKInd <- !is.na(over(activeAK,as(NS, "SpatialPolygons")))
+
+#Plot the North Slope
+plot(coordinates(activeAK),type="n")
+plot(NS,border="blue",add=TRUE)
+
+#Create dataframe that has permafrost data for North Slope
+ALDataFrame = data.frame("SiteCode"=rep(0, 41),"SiteName"=rep(0, 41),"Latitude"=rep(0, 41),"Longitude"=rep(0, 41),
+                         "1990"=rep(0, 41),"1991"=rep(0, 41),"1992"=rep(0, 41),"1993"=rep(0, 41),"1994"=rep(0, 41),
+                         "1995"=rep(0, 41),"1996"=rep(0, 41),"1997"=rep(0, 41),"1998"=rep(0, 41),"1999"=rep(0, 41),
+                         "2000"=rep(0, 41),"2001"=rep(0, 41), "2002"=rep(0,41), "2003"=rep(0,41), "2004"=rep(0,41),
+                         "2005"=rep(0,41), "2006"=rep(0,41), "2007"=rep(0,41), "2008"=rep(0,41), "2009"=rep(0,41),
+                         "2010"=rep(0,41),"2011"=rep(0,41), "2012"=rep(0,41), "2013"=rep(0,41), "2014"=rep(0,41),
+                         "2015"=rep(0,41), "test"=rep(0,41));
+colnames(ALDataFrame)<-c("SiteCode","SiteName","Latitude","Longitude","1990","1991","1992","1993","1994","1995",
+                         "1996","1997","1998","1999","2000","2001","2002","2003","2004","2005","2006",
+                         "2007","2008","2009","2010","2011","2012","2013","2014","2015","test")
+
+#populate dataframe
+ALDataFrame[,1] = c("SiteCode" = activeAK@data$Site_Code[1:41])
+ALDataFrame[,2] = c("SiteCode" = activeAK@data$Site_Name[1:41])
+ALDataFrame[,3] = c("SiteCode" = activeAK@data$Latitude[1:41])
+ALDataFrame[,4] = c("SiteCode" = activeAK@data$Longitude[1:41])
+ALDataFrame[,5] = c("SiteCode" = activeAK@data$F7[1:41])
+ALDataFrame[,6] = c("SiteCode" = activeAK@data$F8[1:41])
+ALDataFrame[,7] = c("SiteCode" = activeAK@data$F9[1:41])
+ALDataFrame[,8] = c("SiteCode" = activeAK@data$F10[1:41])
+ALDataFrame[,9] = c("SiteCode" = activeAK@data$F11[1:41])
+ALDataFrame[,10] = c("SiteCode" = activeAK@data$F12[1:41])
+ALDataFrame[,11] = c("SiteCode" = activeAK@data$F13[1:41])
+ALDataFrame[,12] = c("SiteCode" = activeAK@data$F14[1:41])
+ALDataFrame[,13] = c("SiteCode" = activeAK@data$F15[1:41])
+ALDataFrame[,14] = c("SiteCode" = activeAK@data$F16[1:41])
+ALDataFrame[,15] = c("SiteCode" = activeAK@data$F17[1:41])
+ALDataFrame[,16] = c("SiteCode" = activeAK@data$F18[1:41])
+ALDataFrame[,17] = c("SiteCode" = activeAK@data$F19[1:41])
+ALDataFrame[,18] = c("SiteCode" = activeAK@data$F20[1:41])
+ALDataFrame[,19] = c("SiteCode" = activeAK@data$F21[1:41])
+ALDataFrame[,20] = c("SiteCode" = activeAK@data$F22[1:41])
+ALDataFrame[,21] = c("SiteCode" = activeAK@data$F23[1:41])
+ALDataFrame[,22] = c("SiteCode" = activeAK@data$F24[1:41])
+ALDataFrame[,23] = c("SiteCode" = activeAK@data$F25[1:41])
+ALDataFrame[,24] = c("SiteCode" = activeAK@data$F26[1:41])
+ALDataFrame[,25] = c("SiteCode" = activeAK@data$F27[1:41])
+ALDataFrame[,26] = c("SiteCode" = activeAK@data$F28[1:41])
+ALDataFrame[,27] = c("SiteCode" = activeAK@data$F29[1:41])
+ALDataFrame[,28] = c("SiteCode" = activeAK@data$F30[1:41])
+ALDataFrame[,29] = c("SiteCode" = activeAK@data$F31[1:41])
+ALDataFrame[,30] = c("SiteCode" = activeAK@data$F32[1:41])
+ALDataFrame[,31] = c("SiteCode" = activeAK@data$test[1:41])
+
+
+# now plot active layer sites instide and outside alaska 
+points(activeAK[!activeAKInd, ], pch=1, col="gray")
+points(activeAK[activeAKInd, ], pch=16, col="red")
+
+# there are strange, hidden characters in the data. 
+activeAK$F19 <- as.integer(gsub("\xa0", "", activeAK$F19))
+activeAK$test <- as.integer(gsub("<a0>", "", activeAK$F19))
+
+
+
+#-------------------------------------------------------------------------------------------------------------
+#Finds snow extent where active layer point data is
+#Earth Lab - Project Permafrost
+#By: Ksenia Lepikhina and Jeffery Thompson (mentor)
+#Copy Right
+
+#first snow day (full) median snow depth at active layer depth locations
+fsSnowAtLoc = data.frame("2001"=rep(0, 42), "2002"=rep(0,42), "2003"=rep(0,42), "2004"=rep(0,42),
+                         "2005"=rep(0,42), "2006"=rep(0,42), "2007"=rep(0,42), "2008"=rep(0,42),
+                         "2009"=rep(0,42), "2010"=rep(0,42), "2011"=rep(0,42), "2012"=rep(0,42),
+                         "2013"=rep(0,42),"2014"=rep(0,42), "2015"=rep(0,42), "2016"=rep(0,42));
+colnames(fsSnowAtLoc) <- c("2001", "2002", "2003", "2004",
+                           "2005", "2006", "2007", "2008",
+                           "2009", "2010", "2011", "2012",
+                           "2013","2014", "2015", "2016")
+
+row.names(fsSnowAtLoc) <-c(activeAK@data$Site_Name[activeAKInd == TRUE])
+#last snow day (full) median snow depth at active layer depth locations
+lsSnowAtLoc = data.frame("2001"=rep(0, 42), "2002"=rep(0,42), "2003"=rep(0,42), "2004"=rep(0,42),
+                         "2005"=rep(0,42), "2006"=rep(0,42), "2007"=rep(0,42), "2008"=rep(0,42),
+                         "2009"=rep(0,42), "2010"=rep(0,42), "2011"=rep(0,42), "2012"=rep(0,42),
+                         "2013"=rep(0,42),"2014"=rep(0,42), "2015"=rep(0,42), "2016"=rep(0,42));
+colnames(lsSnowAtLoc) <- c("2001", "2002", "2003", "2004",
+                           "2005", "2006", "2007", "2008",
+                           "2009", "2010", "2011", "2012",
+                           "2013","2014", "2015", "2016")
+row.names(lsSnowAtLoc) <-c(activeAK@data$Site_Name[activeAKInd == TRUE])
+#first snow day (continuous) median snow depth at active layer depth locations
+fsSnowContAtLoc = data.frame("2001"=rep(0, 42), "2002"=rep(0,42), "2003"=rep(0,42), "2004"=rep(0,42),
+                             "2005"=rep(0,42), "2006"=rep(0,42), "2007"=rep(0,42), "2008"=rep(0,42),
+                             "2009"=rep(0,42), "2010"=rep(0,42), "2011"=rep(0,42), "2012"=rep(0,42),
+                             "2013"=rep(0,42),"2014"=rep(0,42), "2015"=rep(0,42), "2016"=rep(0,42));
+colnames(fsSnowContAtLoc) <- c("2001", "2002", "2003", "2004",
+                               "2005", "2006", "2007", "2008",
+                               "2009", "2010", "2011", "2012",
+                               "2013","2014", "2015", "2016")
+row.names(fsSnowContAtLoc) <-c(activeAK@data$Site_Name[activeAKInd == TRUE])
+#last snow day (continuous) median snow depth at active layer depth locations
+lsSnowContAtLoc = data.frame("2001"=rep(0, 42), "2002"=rep(0,42), "2003"=rep(0,42), "2004"=rep(0,42),
+                             "2005"=rep(0,42), "2006"=rep(0,42), "2007"=rep(0,42), "2008"=rep(0,42),
+                             "2009"=rep(0,42), "2010"=rep(0,42), "2011"=rep(0,42), "2012"=rep(0,42),
+                             "2013"=rep(0,42),"2014"=rep(0,42), "2015"=rep(0,42), "2016"=rep(0,42));
+colnames(lsSnowContAtLoc) <- c("2001", "2002", "2003", "2004",
+                               "2005", "2006", "2007", "2008",
+                               "2009", "2010", "2011", "2012",
+                               "2013","2014", "2015", "2016")
+row.names(lsSnowContAtLoc) <-c(activeAK@data$Site_Name[activeAKInd == TRUE])
+
+buf <- c(3)
+
+for (i in 1:length(fileNames)){
+  
+  savedFile = file.path('~/Desktop/modis_data/MODISSnowSeasonality/New', fileNames[i]);
+  NS_snow_depth <- stack(paste(savedFile,sep=""))
+  
+  layer1 <- NS_snow_depth[[1]]
+  layer2 <- NS_snow_depth[[2]]
+  layer3 <- NS_snow_depth[[3]]
+  layer4 <- NS_snow_depth[[4]]
+  layer1[layer1 == 0] <- NA 
+  layer2[layer2 == 0] <- NA
+  layer3[layer3 == 0] <- NA
+  layer4[layer4 == 0] <- NA
+
+  snowAtLocFFS <-extract(layer1,activeAK@coords[activeAKInd,],buffer=buf,fun=median,na.rm=TRUE)
+  snowAtLocLFS <-extract(layer2,activeAK@coords[activeAKInd,],buffer=buf,fun=median,na.rm=TRUE)
+  snowAtLocFCS <-extract(layer3,activeAK@coords[activeAKInd,],buffer=buf,fun=median,na.rm=TRUE)
+  snowAtLocLCS <-extract(layer4,activeAK@coords[activeAKInd,],buffer=buf,fun=median,na.rm=TRUE)
+
+    fsSnowAtLoc[,i] = c(snowAtLocFFS)
+    lsSnowAtLoc[,i] = c(snowAtLocLFS)
+    fsSnowContAtLoc[,i] = c(snowAtLocFCS)
+    lsSnowContAtLoc[,i] = c(snowAtLocLCS)
+}
